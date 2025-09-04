22d342ff688ea1cc847ef8bae93b111394623734
ctrlfisht <ctrlshiftweb@gmail.com>
2025-09-04 15:07:35 +0700

    add some validation

--- src/DirectoryService.Application/Locations/Get/GetLocationsValidator.cs
+++ src/DirectoryService.Application/Locations/Get/GetLocationsValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using DirectoryService.Contracts;
 using FluentValidation;
 using Shared.Errors;
 
@@ -12,5 +13,8 @@ public class GetLocationsValidator : AbstractValidator<GetLocationsQuery>
             .WithMessage(AppErrors.Validation.MustBeGreaterThan("page",0).Serialize());
         RuleFor(q => q.PageSize).GreaterThan(0)
             .WithMessage(AppErrors.Validation.MustBeGreaterThan("pageSize",0).Serialize());
+        RuleFor(q => q.PageSize).LessThan(PaginationConstants.MaxPageSize)
+            .WithMessage(AppErrors.Validation.LengthNotInRange(
+                "pageSize", 1, PaginationConstants.MaxPageSize).Serialize());
     }
 }
