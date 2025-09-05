49d0040bcde2b9d8daac48f4704e2b60537350ce
ctrlfisht <ctrlshiftweb@gmail.com>
2025-09-05 15:44:17 +0700

    1. add GetRootsWithChildren feature 2. add GetDepartmentChildren feature

--- src/DirectoryService.Contracts/PaginationConstants.cs
+++ src/DirectoryService.Contracts/PaginationConstants.cs
@@ -5,4 +5,6 @@ public static class PaginationConstants
     public const int DefaultPageSize = 20;
     public const int MaxPageSize = 50;
     public const int DefaultPageIndex = 1;
+    public const int DefaultPrefetch = 3;
+    public const int MaxPrefetch = 20;
 }
